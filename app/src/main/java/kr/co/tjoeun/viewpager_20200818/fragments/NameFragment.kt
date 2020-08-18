34cb99e10b4fea70a055f9979fd884cef95aa864
조경진 <kj_cho@idealidea.co.kr>
2020-08-18 20:38:10 +0900

    필요한 프래그먼트 3개 추가 작업

---
+++ app/src/main/java/kr/co/tjoeun/viewpager_20200818/fragments/NameFragment.kt
@@ -0,0 +1,20 @@
+package kr.co.tjoeun.viewpager_20200818.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import kr.co.tjoeun.viewpager_20200818.R
+
+class NameFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_name, container, false)
+    }
+
+}
