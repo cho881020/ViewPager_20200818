e9ff980b8e0125d31fadd34021e8c3eff5a9d416
조경진 <kj_cho@idealidea.co.kr>
2020-08-18 21:20:32 +0900

    각 페이지별 제목 설정

--- app/src/main/java/kr/co/tjoeun/viewpager_20200818/adapters/MainViewPagerAdapter.kt
+++ app/src/main/java/kr/co/tjoeun/viewpager_20200818/adapters/MainViewPagerAdapter.kt
@@ -8,6 +8,19 @@ import kr.co.tjoeun.viewpager_20200818.fragments.NameFragment
 import kr.co.tjoeun.viewpager_20200818.fragments.PhoneNumFragment
 
 class MainViewPagerAdapter(fm : FragmentManager) : FragmentPagerAdapter(fm) {
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        if (position == 0) {
+            return "이름"
+        }
+        else if (position == 1) {
+            return "폰번"
+        }
+        else {
+            return "거주지"
+        }
+    }
+
     override fun getItem(position: Int): Fragment {
 
         if (position == 0) {
