32a01045c4b9c0cc857f288669a0074f055a5844
조경진 <kj_cho@idealidea.co.kr>
2020-08-18 20:50:38 +0900

    뷰페이저에서 프래그먼트들을 뿌려주는 작업

---
+++ app/src/main/java/kr/co/tjoeun/viewpager_20200818/adapters/MainViewPagerAdapter.kt
@@ -0,0 +1,28 @@
+package kr.co.tjoeun.viewpager_20200818.adapters
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import kr.co.tjoeun.viewpager_20200818.fragments.AddressFragment
+import kr.co.tjoeun.viewpager_20200818.fragments.NameFragment
+import kr.co.tjoeun.viewpager_20200818.fragments.PhoneNumFragment
+
+class MainViewPagerAdapter(fm : FragmentManager) : FragmentPagerAdapter(fm) {
+    override fun getItem(position: Int): Fragment {
+
+        if (position == 0) {
+            return NameFragment()
+        }
+        else if (position == 1) {
+            return PhoneNumFragment()
+        }
+        else {
+            return AddressFragment()
+        }
+
+    }
+
+    override fun getCount(): Int {
+        return 3
+    }
+}
